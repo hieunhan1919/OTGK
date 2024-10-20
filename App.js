5067522ba06c5aeb2f987480220282893d95f883
H82M19\Student <Student>
2024-10-20 14:12:09 +0700

    giao dien 2

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from './screens/LoginScreen'; 
+import ElectronicsScreen from './screens/ElectronicsScreen'; 
+
 
 
 const Stack = createStackNavigator();
@@ -15,8 +17,22 @@ export default function App() {
           component={LoginScreen} 
           options={{ headerShown: false }} // Ẩn header cho màn hình đăng nhập
         />
-        
+        <Stack.Screen 
+          name="Electronics" 
+          component={ElectronicsScreen} 
+          options={{ 
+            headerTitle: '',
+            headerBackTitleVisible: false, 
+            headerStyle: {
+              elevation: 0, 
+              shadowOpacity: 0, 
+              borderBottomWidth: 0, 
+            },
+            header: () => null,
+          }}
+        />
       </Stack.Navigator>
+      
     </NavigationContainer>
   );
 }
